5986aabf6f0f652cb5a20f9519d7ae44e30dbbbd
MikEight <mickael.4870@gmail.com>
2025-02-28 11:07:40 +0100

    Fixed TailwindCSS installation: - Fully removed and reinstalled TailwindCSS - Verified presence of the binary in `.bin/` - Tested Tailwind application on the project
    Added BaseLayout.astro:
    - Highly optimized SEO (meta, OpenGraph, Twitter, Schema.org)
    - Structured with a slot for dynamic content
    - Integrated metadata dynamically via `config.js`
    
    Added `config.js` file:
    - Centralized SEO and social media info
    - Added structured data for Google (Schema.org)
    - Easier metadata management and modifications"

---
+++ src/config.js
@@ -0,0 +1,33 @@
+const siteConfig = {
+    // Informations générales du site
+    title: "Jimmy Lalin - Boxeur Professionnel 🥊",
+    metaDescription: "Découvrez Jimmy Lalin, l’étoile montante de la boxe ! Parcours, combats, actualités – Suivez son ascension.",
+    metaKeywords: "Jimmy Lalin, boxe, boxeur, combats, sport, athlète, championnat",
+    url: "https://www.jimmylalin.com",
+    ogImage: "https://www.jimmylalin.com/img/og-image.jpg",
+
+    // Réseaux sociaux
+    social: {
+        instagram: "https://www.instagram.com/jimmylalin/",
+        facebook: "https://www.facebook.com/jimmy.lalin.5",
+    },
+
+    // Données structurées Schema.org pour Google
+    structuredData: {
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "Jimmy Lalin",
+        "url": "https://www.jimmylalin.com",
+        "image": "https://www.jimmylalin.com/img/jimmy-profile.jpg",
+        "jobTitle": "Boxeur Professionnel",
+        "sameAs": [
+            "https://www.instagram.com/jimmylalin/",
+            "https://www.facebook.com/jimmy.lalin.5",
+        ],
+        "birthDate": "1996-04-19",
+        "nationality": "Française",
+        "affiliation": "Fédération Française de Boxe, Ambassadeur Sportif Aix-les-Bains",
+    }
+};
+
+export default siteConfig;
