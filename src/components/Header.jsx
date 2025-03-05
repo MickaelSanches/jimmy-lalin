64490615d73eae536fea2ed700356d12a4cd032a
MikEight <mickael.4870@gmail.com>
2025-03-05 18:11:25 +0100

    Improved header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,10 @@ const Header = () => {
   };
 
   return (
-    <header className="relative bg-gradient-to-r from-transparent via-[#3F2876] to-[#3F2876] p-4 lg:px-16 flex justify-between items-center w-full fixed top-0 z-50 h-32 md:h-40 shadow-md">
+    <header className="relative bg-gradient-to-r from-transparent via-[#3F2876] to-[#3F2876] p-4 lg:px-16 flex justify-between items-center w-full fixed top-0 z-50 h-32 md:h-40">
+      {/* LOGO GAUCHE */}
       <a href="/" className="flex items-center">
-        <div className="w-40 h-40 md:w-48 md:h-48 rounded-full border-4 border-white shadow-lg flex items-center justify-center overflow-hidden absolute -bottom-16 left-16">
+        <div className="w-40 h-40 md:w-48 md:h-48 rounded-full border-4 border-white shadow-lg flex items-center justify-center overflow-hidden absolute -bottom-16 left-4 md:left-16">
           <img
             src="/img/jlheader.webp"
             alt="Jimmy Lalin"
@@ -19,16 +20,13 @@ const Header = () => {
         </div>
       </a>
 
-      <h1 className="hidden md:block text-4xl md:text-5xl font-bold bg-gradient-to-r from-black to-[#D1BB34] text-transparent bg-clip-text tracking-widest absolute left-1/2 transform -translate-x-1/2">
-        JIMMY LALIN
-      </h1>
-
-      <nav className="hidden md:flex space-x-4 text-sm">
-        <ul className="flex space-x-4">
+      {/* NAVIGATION DESKTOP */}
+      <nav className="hidden md:flex space-x-6 text-sm">
+        <ul className="flex space-x-6">
           <li>
             <a
               href="/"
-              className="bg-black text-secondary px-4 py-2 rounded-lg hover:bg-secondary hover:text-black transition"
+              className="bg-black text-[#D1BB34] font-bold italic px-6 py-3 rounded-lg hover:bg-[#D1BB34] hover:text-black transition"
             >
               Accueil
             </a>
@@ -36,7 +34,7 @@ const Header = () => {
           <li>
             <a
               href="/combat"
-              className="bg-black text-secondary px-4 py-2 rounded-lg hover:bg-secondary hover:text-black transition"
+              className="bg-black text-[#D1BB34] font-bold italic px-6 py-3 rounded-lg hover:bg-[#D1BB34] hover:text-black transition"
             >
               Combats
             </a>
@@ -44,7 +42,7 @@ const Header = () => {
           <li>
             <a
               href="/biographie"
-              className="bg-black text-secondary px-4 py-2 rounded-lg hover:bg-secondary hover:text-black transition"
+              className="bg-black text-[#D1BB34] font-bold italic px-6 py-3 rounded-lg hover:bg-[#D1BB34] hover:text-black transition"
             >
               Biographie
             </a>
@@ -52,7 +50,7 @@ const Header = () => {
           <li>
             <a
               href="/contact"
-              className="bg-black text-secondary px-4 py-2 rounded-lg hover:bg-secondary hover:text-black transition"
+              className="bg-black text-[#D1BB34] font-bold italic px-6 py-3 rounded-lg hover:bg-[#D1BB34] hover:text-black transition"
             >
               Contact
             </a>
@@ -81,15 +79,16 @@ const Header = () => {
         </button>
       </div>
 
-      {/* NAVIGATION MOBILE + ANIMATION */}
+      {/* NAVIGATION MOBILE */}
       <nav
         className={`absolute top-full right-4 w-2/3 bg-black text-white flex flex-col space-y-4 py-6 px-8 shadow-md md:hidden rounded-lg transform transition-transform duration-300 ease-in-out ${
           isOpen ? "scale-100 opacity-100" : "scale-95 opacity-0"
         }`}
       >
-        <h1 className="text-center text-2xl font-bold bg-gradient-to-r from-[#3F2876] to-[#FFD700] text-transparent bg-clip-text tracking-wide">
+        <h1 className="text-center text-2xl font-bold italic bg-gradient-to-r from-[#3F2876] to-[#FFD700] text-transparent bg-clip-text tracking-wide">
           JIMMY LALIN
         </h1>
+
         <a href="/" className="text-center hover:text-[#D1BB34] transition">
           Accueil
         </a>
